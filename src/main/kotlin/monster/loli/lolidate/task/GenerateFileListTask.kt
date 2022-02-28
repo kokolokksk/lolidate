4fa8a4abcff4b991fbc171cc69012a1ca0681ca9
loveloliii <woshigegefengleng@live.com>
2022-02-28 17:47:59 +0800

    fix bug;add env config file

--- src/main/kotlin/monster/loli/lolidate/task/GenerateFileListTask.kt
+++ src/main/kotlin/monster/loli/lolidate/task/GenerateFileListTask.kt
@@ -6,6 +6,7 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.context.annotation.Configuration
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import java.io.File
@@ -17,11 +18,10 @@ import kotlin.collections.ArrayList
 import kotlin.collections.HashMap
 
 @Component
-@ConfigurationProperties("config.properties")
 class GenerateFileListTask {
-    @Value("\${file_path}")
+    @Value("\${env.dev.config.file_path}")
     lateinit var filePath:String
-    @Value("\${file_list}")
+    @Value("\${env.dev.config.file_list}")
     lateinit var fileList:String
     val log: Logger = LoggerFactory.getLogger(this::class.java)
     private val dateFormat = SimpleDateFormat("HH:mm:ss")
@@ -29,13 +29,15 @@ class GenerateFileListTask {
     fun scanFile(){
         log.info("time:${dateFormat.format(Date())}")
         // 应用版本路径
-        val resultList: ArrayList<HashMap<String, Any>> = LoveLoliiiUtils.searchFile(Paths.get(URI.create(filePath)),fileList)
+        val resultList: ArrayList<HashMap<String, Any>> = LoveLoliiiUtils.searchFile(Paths.get(File(filePath).toURI()),fileList)
         resultList.forEach{
             if(!(it["exist"] as Boolean)){
               val fl = LoveLoliiiUtils.getFileList(Paths.get(URI(it["path"] as String)))
                 val listFile = File(it["path"] as String +File.separator +fileList)
                 listFile.writeBytes(Gson().toJson(fl).toByteArray())
                 log.info("file.list saved in ${it["path"]}")
+            }else{
+                log.info("file.list is exist in ${it["path"]}")
             }
         }
     }
